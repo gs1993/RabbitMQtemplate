92ebaac371207810051add814f8938e8bfaed071
gs1993 <grzegorz.sawinski93@gmail.com>
2019-03-03 13:54:39 +0100

    Client setup

---
+++ RabbitMQtemplate/Message/InfoMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Message
+{
+    public class InfoMessage
+    {
+        public int Type { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
